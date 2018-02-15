68a0e722b574ef51c86dadf66f405367991cea91
Nikhil Joshi <najoshi@ucdavis.edu>
2018-02-15 02:25:51 -0800

    recoding scripts to run as individual jobs rather than task arrays

--- check_runs_loop.pl
+++ check_runs_loop.pl
@@ -16,16 +16,17 @@ while (1) {
     closedir($dh);
 
     foreach $rundir (@dirs) {
-    #    print "$dir\n";
-        # check flags
-        if (-e "$out_base/$rundir/flags/done_flag1" || -e "$out_base/$rundir/flags/done_flag2" || -e "$out_base/$rundir/flags/done_flag3" || -e "$out_base/$rundir/flags/done_flag4" || -e "$out_base/$rundir/flags/done_flag5" || -e "$out_base/$rundir/flags/done_flag6" || -e "$out_base/$rundir/flags/done_flag7" || -e "$out_base/$rundir/flags/done_flag8" || -e "$out_base/$rundir/flags/running_flag") {next;}
 
         ($run_num)=$rundir=~/_run(\d+)/;
         $samplesheet = $run_num . "_SampleSheet.csv";
 
         # check if sample sheet exists
-        if (! -e "$run_base/$rundir/$samplesheet") {next;}
+        if (! -e "$run_base/$rundir/$samplesheet") {
+            print STDERR "Sample sheet not found for $rundir... skipping."
+            next;
+        }
 
+        # check if final bcl file has been generated
         $numcycles=0;
         open($ri, "<$run_base/$rundir/RunInfo.xml");
         while (<$ri>) {
@@ -43,19 +44,31 @@ while (1) {
         }
         close($ri);
 
-
-        # check if final bcl file has been generated
         print STDERR "Checking for $run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz creation...\n";
-        if (-e "$run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz") {
-            #run is ready for bcl2fastq
-            ($run_num)=$rundir=~/_run(\d+)/;
-            $samplesheet = $run_num . "_SampleSheet.csv";
+        if (! -e "$run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz") {next;}
+
+        open($ss,"<$run_base/$rundir/$samplesheet");
+        <$ss>;
+        <$ss>;
+        while (<$ss>) {
+            chomp;
+            @data = split(/,/);
+            $project = $data[8];
+            $flags{$project}=1;
+        }
+        close($ss);
+
+        foreach $project (keys %flags) {
+
+            if (-e "$out_base/$rundir/flags/done__$project" || -e "$out_base/$rundir/flags/running__$project") {next;}
+
+            #project is ready for bcl2fastq
             $outputfolder = "$out_base/$rundir";
-            print STDERR "Running bcl2fastq for $out_base/$rundir...\n";
-            system ("mkdir -p $outputfolder/flags");
-            system ("touch $outputfolder/flags/running_flag");
-            system ("$script_base/split_sample_sheet.pl $run_base/$rundir $run_base/$rundir/$samplesheet $out_base");
-            system ("sbatch --job-name=bcl2fastq_$rundir --output=$outputfolder/slurm_\%a.out $script_base/run_bcl2fastq.slurm $run_base/$rundir $outputfolder $script_base");
+            print STDERR "Running bcl2fastq for $out_base/$rundir/$project...\n";
+            if (! -e "$outputfolder/flags") {system ("mkdir -p $outputfolder/flags");}
+            system ("touch $outputfolder/flags/running_$project");
+            system ("$script_base/get_project_sample_sheet.pl $run_base/$rundir $run_base/$rundir/$samplesheet $out_base $project");
+            system ("sbatch --job-name=bcl2fastq_${rundir}_$project --output=$outputfolder/slurm.$project.out $script_base/run_bcl2fastq.slurm $run_base/$rundir $outputfolder $script_base $outputfolder/$project.SampleSheet.csv $project");
             #system ("run_bcl2fastq.pl $datadir/$dir/RunInfo.xml $datadir/$dir/$samplesheet $datadir/$dir $outputfolder");
         }
     }
---
+++ get_project_sample_sheet.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use File::Basename;
+
+if (scalar(@ARGV) < 4) {
+    print STDERR "Usage: $0 <run directory> <sample sheet> <outdir base> <project ID>\n";
+    exit(1);
+}
+
+$rundir = basename($ARGV[0]);
+$ssfile = $ARGV[1];
+$out_base = $ARGV[2];
+$project_id = $ARGV[3];
+
+#print STDERR "Running split sample sheet with $rundir, $ssfile, $out_base, $project_id\n";
+
+my %sampfiles;
+if (! -e "$out_base/$rundir") {
+    system ("mkdir $out_base/$rundir");
+}
+
+open($samp, "cat $ssfile | sed 's/\\r/\\n/g' | grep -v ^\$ |");
+$dline=<$samp>;
+$header=<$samp>;
+
+open ($outfile,">$out_base/$rundir/$project.SampleSheet.csv");
+print $outfile "$dline$header";
+while (<$samp>) {
+	chomp;
+	@data = split(/,/,$_,20);
+	$project = $data[8];
+
+    if ($data[0] !~ /\d+/) {next;}
+
+    if ($project eq $project_id) {
+		print $outfile "$data[0],$data[1],$rundir-$data[1],".join(',',@data[3 .. $#data])."\n";
+	}
+}
+close($samp);
+close($outfile)
