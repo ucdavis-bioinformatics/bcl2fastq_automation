69eb92bb182dcecf4a8a9fd72c937cd7add70195
Nikhil Joshi <najoshi@ucdavis.edu>
2018-02-15 02:34:49 -0800

    recoding scripts to run as individual jobs rather than task arrays

--- check_runs.pl
+++ check_runs.pl
@@ -5,22 +5,26 @@ $out_base = "/share/biocore/hiseq-fastq";
 $run_base = "/share/illumina/hiseq";
 $script_base = "/share/biocore/joshi/projects/bcl2fastq_automation";
 
+($sec,$min,$hour,$mday,$mon,$year,$wday,$yday,$isdst) = localtime(time);
+$now = sprintf("%04d-%02d-%02d %02d:%02d:%02d", $year+1900, $mon+1, $mday, $hour, $min, $sec);
+print STDERR "Checking for new data ($now)...\n";
+
 opendir ($dh,$run_base);
 @dirs = grep {! /^\.{1,2}$/ && /_run\d+$/} readdir($dh);
 closedir($dh);
 
 foreach $rundir (@dirs) {
-    #print STDERR "$rundir\n";
-    # check flags
-    if (-e "$out_base/$rundir/flags/done_flag1" || -e "$out_base/$rundir/flags/done_flag2" || -e "$out_base/$rundir/flags/done_flag3" || -e "$out_base/$rundir/flags/done_flag4" || -e "$out_base/$rundir/flags/done_flag5" || -e "$out_base/$rundir/flags/done_flag6" || -e "$out_base/$rundir/flags/done_flag7" || -e "$out_base/$rundir/flags/done_flag8" || -e "$out_base/$rundir/flags/running_flag") {next;}
 
     ($run_num)=$rundir=~/_run(\d+)/;
     $samplesheet = $run_num . "_SampleSheet.csv";
 
-#print STDERR "Checking sample sheet $out_base/$rundir/$samplesheet\n";
     # check if sample sheet exists
-    if (! -e "$run_base/$rundir/$samplesheet") {next;}
+    if (! -e "$run_base/$rundir/$samplesheet") {
+        print STDERR "Sample sheet not found for $rundir... skipping."
+        next;
+    }
 
+    # check if final bcl file has been generated
     $numcycles=0;
     open($ri, "<$run_base/$rundir/RunInfo.xml");
     while (<$ri>) {
@@ -38,19 +42,31 @@ foreach $rundir (@dirs) {
     }
     close($ri);
 
-
-    # check if final bcl file has been generated
     print STDERR "Checking for $run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz creation...\n";
-    if (-e "$run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz") {
-        #run is ready for bcl2fastq
-        ($run_num)=$rundir=~/_run(\d+)/;
-        $samplesheet = $run_num . "_SampleSheet.csv";
+    if (! -e "$run_base/$rundir/Data/Intensities/BaseCalls/L00$lanecount/C$numcycles.1/s_${lanecount}_$surfacecount$swathcount$tilecount.bcl.gz") {next;}
+
+    open($ss,"<$run_base/$rundir/$samplesheet");
+    <$ss>;
+    <$ss>;
+    while (<$ss>) {
+        chomp;
+        @data = split(/,/);
+        $project = $data[8];
+        $flags{$project}=1;
+    }
+    close($ss);
+
+    foreach $project (keys %flags) {
+
+        if (-e "$out_base/$rundir/flags/done__$project" || -e "$out_base/$rundir/flags/running__$project") {next;}
+
+        #project is ready for bcl2fastq
         $outputfolder = "$out_base/$rundir";
-        print STDERR "Running bcl2fastq for $out_base/$rundir...\n";
-        system ("mkdir -p $outputfolder/flags");
-        system ("touch $outputfolder/flags/running_flag");
-        system ("$script_base/split_sample_sheet.pl $run_base/$rundir $run_base/$rundir/$samplesheet $out_base");
-        system ("sbatch --job-name=bcl2fastq_$rundir --output=$outputfolder/slurm_\%a.out $script_base/run_bcl2fastq.slurm $run_base/$rundir $outputfolder $script_base");
+        print STDERR "Running bcl2fastq for $out_base/$rundir/$project...\n";
+        if (! -e "$outputfolder/flags") {system ("mkdir -p $outputfolder/flags");}
+        system ("touch $outputfolder/flags/running_$project");
+        system ("$script_base/get_project_sample_sheet.pl $run_base/$rundir $run_base/$rundir/$samplesheet $out_base $project");
+        system ("sbatch --job-name=bcl2fastq_${rundir}_$project --output=$outputfolder/slurm.$project.out $script_base/run_bcl2fastq.slurm $run_base/$rundir $outputfolder $script_base $outputfolder/$project.SampleSheet.csv $project");
         #system ("run_bcl2fastq.pl $datadir/$dir/RunInfo.xml $datadir/$dir/$samplesheet $datadir/$dir $outputfolder");
     }
 }
